f716b347c3735c7476cdea87b0d15b078d8f98d5
jhor pama <jkpama@misena.edu.co>
2019-06-18 10:18:14 -0500

    The status field is added to the collection of subtask

--- src/models/subtask.js
+++ src/models/subtask.js
@@ -4,6 +4,7 @@ const subTasks = new Schema({
     name: { type: String },
     description: { type: String },
     mytask: { type: String },
+    state: { type: Boolean, default: false },
     create_at: { type: Date, default: Date.now },
     //creator: {type: Schema.Types.ObjectId, ref: "task"}
 });
